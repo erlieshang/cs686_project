adf0dca067ce9c06bb25a4e4abc64fc60a2aa929
Erlie Shang <erlie.shang@uwaterloo.ca>
2017-07-15 15:48:30 -0400

    fmp finished

--- cifar10.py
+++ cifar10.py
@@ -5,10 +5,32 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
+from keras.engine.topology import Layer
+import tensorflow as tf
+
+
+class FractionalMaxPooling(Layer):
+    def __init__(self, pooling_ratio, pseudo_random=None, overlapping=None, **kwargs):
+        super(FractionalMaxPooling, self).__init__(**kwargs)
+        self.pooling_ratio = [1.0, pooling_ratio, pooling_ratio, 1.0]
+        self.pseudo_random = pseudo_random
+        self.overlapping = overlapping
+        self.output_x = None
+        self.output_y = None
+
+    def call(self, x):
+        output = tf.nn.fractional_max_pool(x, self.pooling_ratio, self.pseudo_random, self.overlapping)
+        self.output_x = output[0].shape[1].value
+        self.output_y = output[0].shape[2].value
+        return output[0]
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], self.output_x, self.output_y, input_shape[3])
 
 
 class NNBase(object):
-    def __init__(self, x_train, y_train, x_test, y_test, num_classes, epochs=200, data_augmentation=True, batch_size=32):
+    def __init__(self, x_train, y_train, x_test, y_test, num_classes, epochs=200, data_augmentation=True,
+                 batch_size=32):
         self.x_train = x_train.astype('float32') / 255
         self.y_train = keras.utils.to_categorical(y_train, num_classes)
         self.x_test = x_test.astype('float32') / 255
@@ -26,7 +48,8 @@ class NNBase(object):
     def train(self):
         if not self.data_aug:
             print('Not using data augmentation.')
-            self.model.fit(self.x_train, self.y_train, batch_size=self.batch_size, epochs=self.epochs, validation_data=(self.x_test, self.y_test))
+            self.model.fit(self.x_train, self.y_train, batch_size=self.batch_size, epochs=self.epochs,
+                           validation_data=(self.x_test, self.y_test))
         else:
             print('Using real-time data augmentation.')
             datagen = ImageDataGenerator(
@@ -48,17 +71,28 @@ class NNBase(object):
 
 
 class CNN(NNBase):
+    def __init__(self, x_train, y_train, x_test, y_test, num_classes, epochs=200, data_augmentation=True,
+                 batch_size=32, use_fmp=False):
+        self.use_fmp = use_fmp
+        NNBase.__init__(self, x_train, y_train, x_test, y_test, num_classes, epochs, data_augmentation, batch_size)
+
     def _build_model(self):
         model = Sequential()
 
         model.add(Conv2D(32, (3, 3), padding='same', input_shape=self.x_train.shape[1:], activation='relu'))
         model.add(Conv2D(32, (3, 3), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
+        if self.use_fmp:
+            model.add(FractionalMaxPooling(1.44))
+        else:
+            model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.2))
 
         model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
         model.add(Conv2D(64, (3, 3), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
+        if self.use_fmp:
+            model.add(FractionalMaxPooling(1.44))
+        else:
+            model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.2))
 
         model.add(Flatten())
@@ -103,9 +137,10 @@ class DNN(NNBase):
 
 def main():
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-    # cnn = CNN(x_train, y_train, x_test, y_test, 10)
-    dnn = DNN(x_train, y_train, x_test, y_test, 10)
-    dnn.train()
+    cnn = CNN(x_train, y_train, x_test, y_test, 10, use_fmp=True)
+    cnn.train()
+    # dnn = DNN(x_train, y_train, x_test, y_test, 10)
+    # dnn.train()
 
 
 if __name__ == "__main__":
